2297b17e5bcdd0fee093d86c24e95fa9a9374fc8
hikerpig <hikerpigwinnie@gmail.com>
2020-08-14 10:58:03 +0800

    Init
    Change-Id: I3ee8b8280f9243ee9d3a409043ce5d041a1a8af1

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# lark-search-alfre-workflow
-An Alfred workflow to search Lark Space with instant results
+# lark-search-alfred-workflow
+
+An Alfred workflow to search Lark Space with instant results.
+
+Based on notion-search-alfred-workflow, thanks.
+
+Simply type your keyword into Alfred (default: lk) to see instant search results from Lark Docs. Selecting a search result takes you to that page in Lark Docs in your default web browser.
+
+## Installation
+
+
+## Feature
+
+type `lk [doc_name_you_want_to_search]` in Alfred, you will see results coming in if there are matches.
+
+![](https://i.loli.net/2020/08/14/Gx8PQftEpA3WCgF.png)
+
+## Setup
+
+### 1. Get your Lark Domain
+
+Normally it's something like `https://*.larksuite.com` .
+
+### 2. Get cookie session
+
+Goto any Lark Docs page in your browser, make sure you are logged in. Then:
+
+open Devtools -> Applicatoin -> Cookies -> check your lark domain's cookies -> copy the value of `session`
+
+![](https://i.loli.net/2020/08/13/c7eyYaC2quAiOKS.png)
+
+### Set variables in Alfred workflow
+
+Paste those two variables into workflow configuration.
+
+![](https://i.loli.net/2020/08/14/huFpDeo2xNLsRqi.png)
---
+++ lark-search.py
@@ -0,0 +1,116 @@
+# coding=utf-8
+
+import httplib
+import json
+import os
+import os.path
+import struct
+import sys
+import urllib
+import unicodedata
+import re
+
+from urlparse import urlparse
+from datetime import datetime, date
+
+from searchresult import SearchResult
+
+# config
+session = os.environ['session']
+larkDomain = os.environ['larkDomain']
+
+cookie = "session={}".format(session)
+
+def buildSearchPayload():
+    query = {
+        "query": unicodeAlfredQuery,
+        "offset": 0,
+        "count": 9,
+        "candidates_type": 2,
+        "folder_tokens": None,
+        "config_source": 0,
+        "source": "web"
+    }
+
+    jsonData = json.dumps(query)
+    return jsonData
+
+# Get query from Alfred
+alfredQuery = str(sys.argv[1])
+unicodeAlfredQuery = unicodedata.normalize('NFC', alfredQuery.decode('utf-8', 'ignore'))
+
+parsedLarkDomain = urlparse(larkDomain)
+larkDriveHome = "https://{}/drive/home/".format(parsedLarkDomain.netloc)
+referer = larkDriveHome
+
+# Call Space api
+headers = {"Content-type": "application/json",
+           "Referer": referer,
+           "Cookie": cookie}
+conn = httplib.HTTPSConnection(parsedLarkDomain.netloc)
+conn.request("POST", "/space/api/search/refine_search/",
+             buildSearchPayload(), headers)
+response = conn.getresponse()
+
+resData = response.read()
+conn.close()
+
+data = json.loads(resData)['data']
+
+# for debug
+# data = {}
+# with open('results.json') as json_file:
+#     data = json.load(json_file)
+# data = data['data']
+
+ITEM_TYPE_ICONNAME_MAPPING = {
+    2: 'doc.png',
+    3: 'sheet.png',
+}
+
+TITLE_REPLACE_PATTERN = "<em>(.*)</em>"
+
+# Extract search results from lark response
+searchResultList = []
+objs = data['entities']['objs']
+for k in data['tokens']:
+    obj = objs[k]
+    searchResultObject = SearchResult(obj['token'])
+    title = obj['title']
+    title = re.sub(TITLE_REPLACE_PATTERN, r"\1", title)
+    searchResultObject.title = title
+    viewed_time = datetime.fromtimestamp(obj['open_time']).strftime('%H:%M')
+    updated_time = datetime.fromtimestamp(obj['edit_time']).isoformat()
+    searchResultObject.subtitle = "Author: " + obj['author'] + ", You viewed " + viewed_time + ", " + obj['edit_name'] + " updated " + updated_time
+    searchResultObject.link = obj['url']
+    if ITEM_TYPE_ICONNAME_MAPPING.has_key(obj['type']):
+        searchResultObject.icon = "itemicons/" + ITEM_TYPE_ICONNAME_MAPPING.get(obj['type'])
+    searchResultList.append(searchResultObject)
+
+itemList = []
+for searchResultObject in searchResultList:
+    item = {}
+    item["uid"] = searchResultObject.id
+    item["type"] = "default"
+    item["title"] = searchResultObject.title
+    item["arg"] = searchResultObject.link
+    item["subtitle"] = searchResultObject.subtitle
+    if searchResultObject.icon:
+        icon = {}
+        icon["path"] = searchResultObject.icon
+        item["icon"] = icon
+    item["autocomplete"] = searchResultObject.title
+    itemList.append(item)
+
+items = {}
+if not itemList:
+    item = {}
+    item["uid"] = 1
+    item["type"] = "default"
+    item["title"] = "No results - go to Lark Space page"
+    item["arg"] = larkDriveHome
+    itemList.append(item)
+items["items"] = itemList
+items_json = json.dumps(items)
+
+sys.stdout.write(items_json)
